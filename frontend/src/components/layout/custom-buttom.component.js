d75ffec66e404dc880001d3b4c4cd980bc4bdc4e
Waynexia888 <qiwuwaynexia@gmail.com>
2020-02-24 00:37:40 -0800

    styling cart component

--- frontend/src/components/layout/custom-buttom.component.js
+++ frontend/src/components/layout/custom-buttom.component.js
@@ -2,8 +2,8 @@ import React from "react";
 
 import "./custom-buttom.styles.scss";
 
-const CustomButton = ({ children, ...otherProps }) => (
-  <button className='custom-button' {...otherProps}>
+const CustomButton = ({ children, inverted, ...otherProps }) => (
+  <button className={`${inverted ? 'inverted' : ''} custom-button`} {...otherProps}>
     {children}
   </button>
 );
